88404ec89a2d3d403911281349fc3c28b0a56934
Pavel Perezhogin <pperezhogin@gmail.com>
2025-10-02 13:55:47 -0400

    Library finished

--- src/tensor_calculus/tensor_calculus.py
+++ src/tensor_calculus/tensor_calculus.py
@@ -1,4 +1,5 @@
 import xarray as xr
+import numpy as np
 import xgcm
 import itertools
 import copy
@@ -7,7 +8,10 @@ import copy
 
 default_indices_names = ["i", "j", "k", "m", "n", "l", "o", "s", "t", "p", "q", "r", "a", "b"]
 
-def transpose_data(array):
+def return_dims(array):
+    return [idx for idx in array.dims if idx in default_indices_names]
+
+def transposition_data(array):
     '''
     Note transposing does not relabel data
     but simply changes its representation in xarray
@@ -24,50 +28,33 @@ def transpose_data(array):
             dims.append(dim)
     return array.transpose(*dims)
 
+def transpose_tensor(array, pair):
+    return array.copy().swap_dims({pair[0]:pair[1], pair[1]:pair[0]})
+
+def symmetrize_tensor(_array, sym_axes):
+    array = _array * 0
+    permutations = list(itertools.permutations(sym_axes))
+    for ax in permutations:
+        d = {key:val for key,val in zip(sym_axes, ax)}
+        array += _array.copy().swap_dims(d)
+    return array / len(list(permutations))
+
 def ddx(array):
     '''
     d/dx operator applied in center points
     '''
     out = param.wet * grid.interp(param.wet_u * grid.diff(param.wet * array,'X') / param.dxCu, 'X')
-    return transpose_data(out)
+    return transposition_data(out)
 
 def ddy(array):
     '''
     d/dy operator applied in center points
     '''
     out = param.wet * grid.interp(param.wet_v * grid.diff(param.wet * array,'Y') / param.dyCv, 'Y')
-    return transpose_data(out)
-
-def pair_is_equal(pair1, pair2, symmetric_indices_sets):
-    '''
-    Service two function which decides if
-    contraction of pair1 of indices and over pair2
-    is identical given the symmetry of the tensor
-    symmetric_indices_sets = list(list())
-    '''
-    if set(pair1) == set(pair2):
-        return True
-
-    # This operator returns a full list of entries which
-    # are different in two sets. In a case of symmetric 
-    # tensors these should be two elements and
-    # both should lie in symmetric_indices_set
-    different_indices = list(set(pair1) ^ set(pair2))
-    
-    # We check each set indipendently
-    # if different indices correspond to any of sets
-    # then the pairs are equal
-    for symmetric_indices in symmetric_indices_sets:
-        idx_in_symmetric_set = [False] * len(different_indices)
-        for j, idx in enumerate(different_indices):
-            if idx in symmetric_indices:
-                idx_in_symmetric_set[j] = True
-        if all(idx_in_symmetric_set):
-            return True
-    return False
+    return transposition_data(out)
 
 class Tensor():
-    def __init__(self, array, label='', symmetric_indices_sets=[]):
+    def __init__(self, array, label='', hash_array=None):
         '''
         array is xarray
         label is the Latex code describing the 
@@ -75,16 +62,19 @@ class Tensor():
         should not be used in other sense
         all tensor indices present in array must
         be specified in label as well
+
+        hash_array is used to perform cheap calculations
+        and determine tensors which are similar
         '''
-        self.array = transpose_data(array)
-        self.verbose = False
+        self.array = transposition_data(array)
         self.label = label
-        # All tuples of indices which show symmetry of the tensor
-        # for example d3 u / dx_i dx_j dx_k is symmetric in indices
-        # i,j,k and thus should pass a tuple ('i', 'j', 'k')
-        # Temporarily, this set is should be set directly
-        self.symmetric_indices_sets = symmetric_indices_sets
-    
+        if hash_array is None:
+            dims = return_dims(self.array)
+            # We do not assume any symmetry here
+            self.hash_array = xr.DataArray(np.random.randn(*[2]*len(dims)), dims=dims)
+        else:
+            self.hash_array = transposition_data(hash_array)
+
     # Service functions
     def _repr_latex_(self):
         '''
@@ -102,22 +92,30 @@ class Tensor():
         Modifies both array and label
         '''
         label = self.label
+        array = self.array.copy()
+        hash_array = self.hash_array.copy()
         for idx_old, idx_new in zip(self.dims(), default_indices_names):
-            self.array = self.array.rename({idx_old:idx_new})
+            array = array.rename({idx_old:idx_new})
+            hash_array = hash_array.rename({idx_old:idx_new})
             label = label.replace(idx_old, idx_new)
-        return Tensor(self.array, label)
+        return Tensor(array, label, hash_array)
 
     def dims(self):
         '''
         Returns list of tensor indices
         '''
-        return [idx for idx in self.array.dims if idx in default_indices_names]
+        idx_array = return_dims(self.array)
+        idx_hash_array = return_dims(self.hash_array)
+        if set(idx_array) != set(idx_hash_array):
+            raise ValueError(f"Mismatch between array dims and hash_array dims: {idx_array} vs {idx_hash_array}")
+        
+        return idx_array
 
     def copy(self):
         '''
         Copy tensor object and associated data
         '''
-        return Tensor(self.array.copy(), self.label, copy.deepcopy(self.symmetric_indices_sets))
+        return Tensor(self.array.copy(), copy.deepcopy(self.label), self.hash_array.copy())
     
     def transpose(self, pair):
         '''
@@ -125,140 +123,139 @@ class Tensor():
         pair: tuple of two indices (e.g. ("i", "j"))
         '''
         # Swap dims in the underlying xarray
-        arr_new = self.array.rename({pair[0]: "tmp"}).rename({pair[1]: pair[0]}).rename({"tmp": pair[1]})
+        array = transpose_tensor(self.array, pair)
+        hash_array = transpose_tensor(self.hash_array, pair)
 
         # Update LaTeX label: swap pair[0] and pair[1]
         tmp_symbol = "__TMP__"
+        label = copy.deepcopy(self.label)
         new_label = (
-            self.label
+            label
             .replace(pair[0], tmp_symbol)
             .replace(pair[1], pair[0])
             .replace(tmp_symbol, pair[1])
         )
 
-        return Tensor(arr_new, new_label)
+        return Tensor(array, label, hash_array)
     
-    def contract_to_rank(self, rank=1):
+    def contract_to_rank_one(self):
         """
-        Recursively contract tensor indices until it has the desired rank.
-        Returns a list of Tensor objects of the target rank.
+        Non-recursive contraction to rank-1 tensors.
+        Only keep unique results (by hash_array / 1D array comparison).
+        Returns a list of Tensor objects of rank 1.
         """
         n = len(self.dims())
-
-        # Base case: impossible
-        if n < rank or (n - rank) % 2 != 0:
-            return []
-
-        if n == rank:
+        
+        if n == 1:
             return [self]
 
-        # Base case: one contraction left
-        if n - rank == 2:
-            return self.list_of_all_contractions()
+        if n%2 == 0:
+            return []
+        
+        # Number of pairs to contract
+        n_pairs = (n - 1) // 2
+        
+        # Free indices to contract
+        free_indices = self.dims()
+        
+        # Generate all unordered sets of indices to contract
+        # Each combination is a list of pairs of indices
+        all_pairs = list(itertools.combinations(free_indices, 2))
 
-        # Recursive case
+        # Generate all possible sets of indices without repetition
+        # Repetition is enforces by the fact that all indices are different
+        sets_of_pairs = []
+        for set_of_pairs in list(itertools.combinations(all_pairs, n_pairs)):
+            '''
+            Keep only pairs which have all different indices
+            '''
+            all_indices = set([idx for pair in set_of_pairs for idx in pair])
+            if len(all_indices) == n_pairs * 2:
+                sets_of_pairs.append(set_of_pairs)
+        
+        # Keep only those sets of pairs which return unique hash
+        unique_hashes = []
         results = []
-        for contracted in self.list_of_all_contractions():
-            results.extend(contracted.contract_to_rank(rank=rank))
+        for pair_set in sets_of_pairs:
+            # Perform contractions
+            hash_array = self.hash_array.copy()
+            for pair in pair_set:
+                hash_array = sum(hash_array.isel({pair[0]: i, pair[1]: i}) for i in range(2)).compute()
+
+            # Convert to 1D numpy array
+            hash_array = np.array(hash_array)
+            
+            # Check uniqueness
+            if not any(np.allclose(hash_array, h) for h in unique_hashes):
+                unique_hashes.append(hash_array)
+                tensor = self
+                for pair in pair_set:
+                    tensor = tensor.contract(pair)
+                results.append(tensor)
         
         return results
 
-    def list_of_all_contractions(self):
-        '''
-        Takes the current tensor and if its
-        rank is higher or equal than 2,
-        finds all possible contractions over
-        two indices filtering out identical
-        ones given by symmetry of the tensor
-        '''
-
-        if len(self.dims()) < 2:
-            return []
-
-        # All unordered combinations
-        pairs = [pair for pair in itertools.combinations(self.dims(),2)]
-
-        # Keep only pairs which remain after applying symmetry conditions
-        pairs_filtered = []
-
-        for pair1 in pairs:
-            append = True
-            for pair2 in pairs_filtered:
-                if pair_is_equal(pair1, pair2, self.symmetric_indices_sets):
-                    append=False
-            
-            if append:
-                pairs_filtered.append(pair1)
-
-        # Compute contractions
-        output = []
-        for pair in pairs_filtered:
-            output.append(self.contract(pair))
-
-        return output
-
-    def contract(self, *pairs):
-        out = self
-        for pair in pairs:
-            out = out._contract(pair)
-        return out
-        
-    def _contract(self, pair):
+    def contract(self, pair):
         '''
         Contract tensor over a pair of indices
         By convention, contracted indices are big in the label
         and thus they are not anymore free dimensions of the tensor
         '''
-        out = 0
-        for i in range(2):
-            out += self.array.isel({pair[0]:i, pair[1]:i})
+
+        # Contract array
+        array = sum(self.array.isel({pair[0]: i, pair[1]: i}) for i in range(2))
+        # Contract hash_array
+        hash_array = sum(self.hash_array.isel({pair[0]: i, pair[1]: i}) for i in range(2))
+        
         # pick a single uppercase letter from the pair[0]
         contracted = pair[0].upper()
         # replace both indices in the label with the uppercase one
-        new_label = self.label.replace(pair[0], contracted).replace(pair[1], contracted)
+        label = self.label.replace(pair[0], contracted).replace(pair[1], contracted)
 
-        # Remove contracted indices from the list of symmetric indices
-        new_symmetric_indices_sets = [[s for s in sub if s not in pair] for sub in self.symmetric_indices_sets]
-        return Tensor(transpose_data(out), new_label, new_symmetric_indices_sets)
+        return Tensor(array, label, hash_array)
 
-    def __add__(self, _tensor2):
-        return Tensor(self.array+_tensor2.array, self.label+'+'+_tensor2.label)
+    def __add__(self, tensor2):
+        return Tensor(self.array+tensor2.array, self.label+'+'+tensor2.label, self.hash_array+tensor2.hash_array)
 
-    def __sub__(self, _tensor2):
-        return Tensor(self.array-_tensor2.array, self.label+'-'+_tensor2.label)
+    def __sub__(self, tensor2):
+        return Tensor(self.array-tensor2.array, self.label+'-'+tensor2.label, self.hash_array-tensor2.hash_array)
 
-    def __mul__(self, _tensor2):
+    def __mul__(self, tensor2):
         '''
         Multiplies two tensors as outer product,
         i.e. without repeating indices
         Main code handles that indices are indeed not reepeting
         '''
-        tensor2 = _tensor2.copy()
-
+        # Indices in the left tensor
         idx_set1 = self.dims()
+        # Indices in the right tensor
         idx_set2 = tensor2.dims()
 
-        # We rename indices of the second tensor
+        if len(idx_set1) + len(idx_set2) > len(default_indices_names):
+            raise ValueError(f"So many dimensions is not supported")
+
+        # Identify intices in the second tensor to be renamed
+        # as they are identical to those in the left tensor
         idx_rename = [idx for idx in idx_set2 if idx in idx_set1]
 
         # Empty set of indices not yet used in these tensor
         idx_empty = [idx for idx in default_indices_names if idx not in idx_set1 and idx not in idx_set2]
         
-        label2 = tensor2.label
-        symmetric_indices_sets2 = tensor2.symmetric_indices_sets
-        for i,j in zip(idx_rename, idx_empty):
-            tensor2.array = tensor2.array.rename({i:j})
-            label2 = label2.replace(i, j)
-            symmetric_indices_sets2 = [[s.replace(i, j) for s in sub] for sub in symmetric_indices_sets2]
-        
-        label1 = f'({self.label})'
-        label2 = f'({label2})'
+        # Rename indices
+        array2 = tensor2.array.copy()
+        hash_array2 = tensor2.hash_array.copy()
+        label2 = copy.deepcopy(tensor2.label)
 
-        out = self.array * tensor2.array
-        if self.verbose:
-            print('Final set of indices', [idx for idx in out.dims if idx in default_indices_names])
+        for i,j in zip(idx_rename, idx_empty):
+            array2 = array2.rename({i:j})
+            hash_array2 = hash_array2.rename({i:j})
+            label2 = label2.replace(i, j)
+
+        array = self.array * array2
+        hash_array = self.hash_array * hash_array2
+        label = f'({self.label})({label2})'
             
-        return Tensor(out, label1+label2,self.symmetric_indices_sets+symmetric_indices_sets2)
+        return Tensor(array, label, hash_array)
 
     def diff(self):
         '''
@@ -280,3 +277,13 @@ class Tensor():
         """
         arr = xr.concat([u, v], dim='i')
         return cls(arr, label=label)
+
+    def set_symmetric_indices(self, dims):
+        '''
+        This function modifies hash_array to be symmetric over the
+        set of indices. This does not apply to the array itself
+        as we assume that it is already symmetric in these indices
+
+        As this is simple operation updating hash_array, we do it in-place
+        '''
+        self.hash_array = symmetrize_tensor(self.hash_array, dims)
